e351836aa22e29474c32c0120bce06cd7045d9c5
bRad <brad@humanenginuity.com>
2017-01-23 05:20:16 -0800

    range_vec_with_one_valid_range_has_len_one test: PASS

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+#![feature(relaxed_adts)]
 //#[macro_use] extern crate laconic;
 pub mod range_vec;
--- src/range_vec/unit_tests/mod.rs
+++ src/range_vec/unit_tests/mod.rs
@@ -6,3 +6,10 @@ fn virgin_range_vec_has_zero_len()
     let rangeVec = RangeVec::new();
     assert!(rangeVec.len() == 0);
 }
+
+#[test]
+fn range_vec_with_one_valid_range_has_len_one()
+{
+    let rangeVec = RangeVec {0: vec![(1..101)]};
+    assert!(rangeVec.len() == 1);
+}
